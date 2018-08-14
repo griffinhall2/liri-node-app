0d2d122f49b57cda827d8626662a4ffe54707983
griffinhall2 <griffin.hall@yahoo.com>
2018-08-13 22:19:25 -0400

    keys

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is laded');
+
+exports.twitter = {
+    consumer_key: process.env.Jy7qGeJcq2AmMMgQE0BzxgAih,
+    consumer_secret: process.env.PeC8RvSncsVkvG1lsT92tT78sIT20O9IKOG5pwRuf3GXN3w2yY,
+    access_token_key: process.env.1028315013575045120-YeJFYhenW8wgqu44o1JL8pk7XRHCEd,
+    access_token_secret: process.env.r1KNpFSrGxLYZmK5ud0HIquiNoOTlYB6FG6St93wPXVaq
+};
+
+exports.spotify = {
+    id: process.env.dfd42385d7f64ab7b2f59c2374afea27,
+    secret: process.env.8f9a0926d5d94e8284b95d87a7f24aeb
+};
+
